330cb521e28bbbd0db9523d91a4b8960074f937b
Aaron Cleveland <toscleveland@gmail.com>
2020-11-12 15:33:30 -0600

    Initial Commit

---
+++ SwiftUI-AppleFrameworks/SwiftUI_AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppleFrameworksApp.swift
+//  SwiftUI-AppleFrameworks
+//
+//  Created by 09299775 on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
