db84280ba812bab9d7b2a154d3e1ea2b57ba6a8c
Aaron Cleveland <toscleveland@gmail.com>
2020-11-13 16:24:44 -0600

    Added SafariView sheet

---
+++ SwiftUI-AppleFrameworks/SafariView.swift
@@ -0,0 +1,23 @@
+//
+//  SafariView.swift
+//  SwiftUI-AppleFrameworks
+//
+//  Created by 09299775 on 11/13/20.
+//
+
+import Foundation
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {}
+    
+    
+}
