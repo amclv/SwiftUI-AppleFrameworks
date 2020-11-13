583c0bd0dc3403c020023e92e3296b7d8cfb348b
Aaron Cleveland <toscleveland@gmail.com>
2020-11-13 02:45:44 -0600

    Connected Views together

---
+++ SwiftUI-AppleFrameworks/AFButton.swift
@@ -0,0 +1,29 @@
+//
+//  AFButton.swift
+//  SwiftUI-AppleFrameworks
+//
+//  Created by 09299775 on 11/12/20.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 280, height: 50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton(title: "Test Title")
+    }
+}
---
+++ SwiftUI-AppleFrameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  FrameworkGridViewModel.swift
+//  SwiftUI-AppleFrameworks
+//
+//  Created by 09299775 on 11/12/20.
+//
+
+import Foundation
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    
+    var selectedFramework: Framework? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    
+    @Published var isShowingDetailView = false
+    
+}
